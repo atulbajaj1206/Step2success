4dd0c13ed95f558818329f64a9ddaceac84730f9
atulbajaj1206 <62832632+atulbajaj1206@users.noreply.github.com>
2020-04-05 14:55:03 +0530

    5
    5

--- Pandas/read/temp.py
+++ Pandas/read/temp.py
@@ -3,16 +3,15 @@ import pandas
 #pip install pandas on cmd
 
 #reading file in the same folder or provide the path
-df=pandas.read_excel("input.xlsx",index_col='switch')
-
-
+#df=pandas.read_excel("input.xlsx",index_col='switch')
 
 #use this oine if your headingg is not placed in by deafult o position
-'''df=pandas.read_excel("input.xlsx",header=1)'''
-a=(df.loc['Txn23'])
-print(a)
+df=pandas.read_excel("input.xlsx",header=1)
+print(df)
+'''a=(df.loc['Txn23'])
+b=(df.loc['Txn24'])
+print(b)
 print('---')
 print(a['output'])
 # or
-print(a[2])
-
+print(a[2])'''
